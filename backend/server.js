ce1246ccee8cf0283593f65534d9525611bedf6c
DGLOliveira <dgloliveira90@gmail.com>
2026-06-05 17:30:00 +0000

    Database setup

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import morgan from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
 
+import {sql} from "./config/db.js"
 import productRoute from "./routers/products.route.js"
 
 
@@ -20,11 +21,29 @@ app.use(morgan("dev"));// log the requests
 
 app.use("/api/products",productRoute)
 
+async function initDB(){
+    try {
+    await sql`
+      CREATE TABLE IF NOT EXISTS products (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        image VARCHAR(255) NOT NULL,
+        price DECIMAL(10, 2) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      )
+    `;
+
+    console.log("Database initialized successfully");
+  } catch (error) {
+    console.log("Error initDB", error);
+  }
+}
+
 app.get("/test",(req, res)=>{
     console.log(res.getHeaders())
     res.send("Test Page")
 })
 
-app.listen(PORT, ()=>{
+initDB().then(app.listen(PORT, ()=>{
     console.log("Server is running in port " + PORT)
-})
+}))
