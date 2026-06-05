b14b9ffb252fdfa1b16233a4abeda981e3e60b8b
DGLOliveira <dgloliveira90@gmail.com>
2026-06-05 19:02:41 +0000

    Implemented basic protection, rate limiting and bot detection

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 
 import {sql} from "./config/db.js"
+import { arcjetMiddleware } from "./lib/arcjet.js"
 import productRoute from "./routers/products.route.js"
 
 
@@ -18,8 +19,37 @@ app.use(cors());
 app.use(helmet());// helmet is a security middleware that helps you protect your app by setting various HTTP headers
 app.use(morgan("dev"));// log the requests
 
+app.use(async (req, res, next) => {
+  try {
+    const decision = await arcjetMiddleware.protect(req, {
+      requested: 1, // specifies that each request consumes 1 token
+    });
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        res.status(429).json({ error: "Too Many Requests" });
+      } else if (decision.reason.isBot()) {
+        res.status(403).json({ error: "Bot access denied" });
+      } else {
+        res.status(403).json({ error: "Forbidden" });
+      }
+      return;
+    }
 
-app.use("/api/products",productRoute)
+    // check for spoofed bots
+    if (decision.results.some((result) => result.reason.isBot() && result.reason.isSpoofed())) {
+      res.status(403).json({ error: "Spoofed bot detected" });
+      return;
+    }
+
+    next();
+  } catch (error) {
+    console.log("Arcjet error", error);
+    next(error);
+  }
+});
+
+
+app.use("/api/products", productRoute)
 
 async function initDB(){
     try {
