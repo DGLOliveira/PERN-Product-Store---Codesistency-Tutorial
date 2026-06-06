394eeeb87498084f65758c7454b0ef65f60ae918
DGLOliveira <dgloliveira90@gmail.com>
2026-06-06 14:31:59 +0000

    Navbar component setup

---
+++ frontend/src/components/ThemeSelector.component.jsx
@@ -0,0 +1,14 @@
+import { PaletteIcon } from "lucide-react";
+
+
+function ThemeSelector() {
+
+    
+  return (
+      <button tabIndex={0} className="btn btn-ghost btn-circle">
+        <PaletteIcon className="size-5" />
+      </button>
+  )
+}
+
+export default ThemeSelector
