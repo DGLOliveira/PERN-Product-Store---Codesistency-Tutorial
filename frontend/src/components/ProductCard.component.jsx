a7e592538b679d893d9fdf86d062a35bff8099ec
DGLOliveira <dgloliveira90@gmail.com>
2026-06-07 16:56:10 +0000

    Delete product function created

--- frontend/src/components/ProductCard.component.jsx
+++ frontend/src/components/ProductCard.component.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { useProductStore } from "../store/product.store.jsx";
 
 function ProductCard({ product }) {
-  //const { deleteProduct } = useProductStore();
+  const { deleteProduct } = useProductStore();
   return (
     <div className="card bg-base-100 shadow-xl hover:shadow-2xl transition-shadow duration-300">
       {/* PRODUCT IMAGE */}
@@ -28,6 +28,7 @@ function ProductCard({ product }) {
 
           <button
             className="btn btn-sm btn-error  btn-outline"
+            onClick={() => deleteProduct(product.id)}
           >
             <Trash2Icon className="size-4" />
           </button>
--- frontend/src/store/product.store.jsx
+++ frontend/src/store/product.store.jsx
@@ -5,22 +5,37 @@ const BASE_URL = "http://localhost:3000/api";
 
 export const useProductStore = create((set, get) => ({
     products: [],
-    loading:false,
+    loading: false,
     error: null,
 
     fetchProducts: async () => {
-        set({loading: true});
+        set({ loading: true });
         try {
             const response = await axios.get(`${BASE_URL}/products`);
-            set({products: response.data.data, error: null});
+            set({ products: response.data.data, error: null });
         } catch (error) {
-            if(error.statusCode === 429){
-                set({error: "Rate limit exceeded"});
-            }else{
-                set({error: "Something went wrong"});
+            if (error.statusCode === 429) {
+                set({ error: "Rate limit exceeded", products: [] });
+            } else {
+                set({ error: "Something went wrong" });
             }
-        }finally{
-            set({loading: false});
+        } finally {
+            set({ loading: false });
         }
-    }
+    },
+
+    deleteProduct: async () => {
+        set({ loading: true });
+        try {
+            await axios.delete(`${BASE_URL}/api/products/${id}`);
+            set((prev) => ({ products: prev.products.filter((product) => product.id !== id) }));
+            toast.success("Product deleted successfully");
+        } catch (error) {
+            console.log("Error in deleteProduct function", error);
+            toast.error("Something went wrong");
+        } finally {
+            set({ loading: false });
+        }
+    },
+
 }));
